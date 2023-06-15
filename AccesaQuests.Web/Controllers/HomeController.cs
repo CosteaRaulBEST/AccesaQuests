60fdb347ca825aeb12cbada45d2dd0fa26700fcf
DESKTOP-UE1F882\coste <costearaul3232@gmail.com>
2023-06-15 21:11:13 +0300

    added contact info page

--- AccesaQuests.Web/Controllers/HomeController.cs
+++ AccesaQuests.Web/Controllers/HomeController.cs
@@ -40,6 +40,10 @@ namespace AccesaQuests.Web.Controllers
     {
         return View();
     }
+    public IActionResult Contact()
+    {
+        return View();
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
