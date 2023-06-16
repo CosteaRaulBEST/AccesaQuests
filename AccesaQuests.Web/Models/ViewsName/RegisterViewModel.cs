db8e9220b3de4b26d23c7e45d3a61f2a047844a7
DESKTOP-UE1F882\coste <costearaul3232@gmail.com>
2023-06-16 16:19:10 +0300

    added register server side validation

--- AccesaQuests.Web/Models/ViewsName/RegisterViewModel.cs
+++ AccesaQuests.Web/Models/ViewsName/RegisterViewModel.cs
@@ -1,11 +1,18 @@
-﻿namespace AccesaQuests.Web.Models.ViewsName
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AccesaQuests.Web.Models.ViewsName
 {
     public class RegisterViewModel
     {
+        [Required] 
         public string UserName { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
-
+        [Required]
+        [MinLength(6 ,ErrorMessage ="Password has to be eat least 6 characthers")]
         public string Password { get; set;}
+        [Required]
         public string ConfirmPassword { get; set; }
     }
 }
