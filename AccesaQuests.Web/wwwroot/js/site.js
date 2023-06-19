ab4535a6f8338fe008b2f1dc078f45951d9738c5
DESKTOP-UE1F882\coste <costearaul3232@gmail.com>
2023-06-20 00:13:25 +0300

    added responive navbar kinda + icons

--- AccesaQuests.Web/wwwroot/js/site.js
+++ AccesaQuests.Web/wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function toggleMenu() {
+    var menuItems = document.querySelector('.menu-items');
+    menuItems.style.display = (menuItems.style.display === 'none') ? 'block' : 'none';
+}
